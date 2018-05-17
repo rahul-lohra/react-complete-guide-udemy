8a76c5e989f6e2d311d9b52e03b5ea4a194fe66a
rahul_lohra <tgunix@gmail.com>
2018-05-17 23:37:20 +0530

    use map funcitons to iterate over persons and show it in a div

--- src/App.js
+++ src/App.js
@@ -53,14 +53,11 @@ class App extends Component {
         if(this.state.showPersons){
             persons = (
                     <div>
-                        <Preson name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                        <Preson
-                            name={this.state.persons[1].name} age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this,"Barnes")}
-                            change={this.nameChangeHandler}
-                        >
-                            My hobbies : Sleeping</Preson>
-                        <Preson name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                        {
+                            this.state.persons.map(person =>{
+                                return <Preson name = {person.name} age={person.age}/>
+                            })
+                        }
                     </div>
             );
         }
